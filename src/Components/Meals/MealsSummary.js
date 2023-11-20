9bdf88890eebb8616b7b1f92babfbc2a85e0cf4c
Madhankumar11 <madhankumarraja6@gmail.com>
2023-11-20 03:22:47 -0800

    first commit

---
+++ src/Components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Welcome To, Madhankumar Food World! </h2>
+     
+    </section>
+  );
+};
+
+export default MealsSummary;
